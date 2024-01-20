08013504e9e801393dea0d8173f17efe86d68532
Gabs Pessoal <bieelvii13@gmail.com>
2024-01-19 23:05:13 -0300

    fix ajeitado o alinhamento

--- view/Header.php
+++ view/Header.php
@@ -8,7 +8,12 @@
     <title><?= isset($title) ? ucfirst($title) . " - " . translate('Bieelvi Finance') : translate('Bieelvi Finance') ?></title>
 
     <!-- JS essencial -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script 
+        src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" 
+        integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" 
+        crossorigin="anonymous" 
+        referrerpolicy="no-referrer">
+    </script>
     <script src="js/ResizeWidth.js"></script>
 
     <!-- JS nao essencial -->
@@ -19,7 +24,12 @@
     <link rel="icon" type="image/x-icon" href="/favicon.ico">
 
     <link rel="stylesheet" href="/css/reset.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link 
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" 
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" 
+        crossorigin="anonymous"
+    >
     <link rel="stylesheet" href="/css/variables.css">
     <link rel="stylesheet" href="/css/header.css">
     <?php if (is_file(__DIR__ . "/../public/css/{$title}.css")): ?>
@@ -41,17 +51,37 @@
                 <img src="/favicon.ico" alt="<?= translate('Page Logo Alt') ?>">
             </a>
 
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <button 
+                class="navbar-toggler" 
+                type="button" 
+                data-bs-toggle="collapse" 
+                data-bs-target="#navbarNavAltMarkup" 
+                aria-controls="navbarNavAltMarkup" 
+                aria-expanded="false" 
+                aria-label="Toggle navigation"
+            >
                 <span class="navbar-toggler-icon"></span>
             </button>
 
             <div class="collapse navbar-collapse justify-content-between" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
                     <?php if (\Financas\Helper\Session::has()) : ?>
-                        <a class="nav-link <?= $title == 'report' ? 'active border rounded border-white' : '' ?>" aria-current="page" href="/report"><?= translate('Reports') ?></a>
-                        <a class="nav-link <?= $title == 'farmer' ? 'active border rounded border-white' : '' ?>" aria-current="page" href="/farmer"><?= translate('Farmer') ?></a>
+                        <a 
+                            class="nav-link <?= $title == 'report' ? 'active border rounded border-white' : '' ?>" 
+                            aria-current="page" 
+                            href="/report"><?= translate('Reports') ?>
+                        </a>
+                        <a 
+                            class="nav-link <?= $title == 'farmer' ? 'active border rounded border-white' : '' ?>" 
+                            aria-current="page" 
+                            href="/farmer"><?= translate('Farmer') ?>
+                        </a>
                         <?php if($_SESSION['logged']->getType() == \Financas\Enum\UserType::ADMIN->value) : ?>
-                            <a class="nav-link <?= $title == 'products' ? 'active border rounded border-white' : '' ?>" aria-current="page" href="/products"><?= translate('Products') ?></a>
+                            <a 
+                                class="nav-link <?= $title == 'products' ? 'active border rounded border-white' : '' ?>" 
+                                aria-current="page" 
+                                href="/products"><?= translate('Products') ?>
+                            </a>
                         <?php endif; ?>
                     <?php endif; ?>
                 </div>
